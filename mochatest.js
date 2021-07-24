c2bf866bc6798190485b736eb426cb0e90ecc3fd
Noye Samuel <82453244+samuelnoye@users.noreply.github.com>
2021-07-24 11:01:25 +0000

    initial commit

---
+++ mochatest.js
@@ -0,0 +1,33 @@
+const assert = require('assert');
+const fs = require('fs');
+
+describe('appendFileSync', () => {
+    const path = './message.txt';
+
+    it('writes a string to text file at given path name', () => {
+
+        // Setup
+        const str = 'Hello Node.js';
+
+        // Exercise: write to file
+        fs.appendFileSync(path, str);
+
+        // Verify: compare file contents to string
+        const contents = fs.readileSync(path);
+        assert.ok(contents.toString() === str);
+
+        // Teardown: delete path
+
+
+        afterEach(() => {
+            // teardown goes here
+            fs.unlinkSync(path);
+        });
+
+        it('.sample', () => {
+            // test goes here
+        });
+
+
+    });
+});
